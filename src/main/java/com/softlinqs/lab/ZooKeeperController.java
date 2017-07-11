728bb6f52a404b451ead9330f6090f70331080ee
krasimir <krasimir.georgiev@softlinqs.com>
2017-07-11 07:46:39 +0100

    Kafka/Zookeeper local

---
+++ src/main/java/com/softlinqs/lab/ZooKeeperController.java
@@ -0,0 +1,36 @@
+package com.softlinqs.lab;
+
+import org.apache.zookeeper.server.ServerConfig;
+import org.apache.zookeeper.server.ZooKeeperServerMain;
+import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ZooKeeperController {
+
+    public static void main(String[] args) {
+        Properties startupProperties = new Properties();
+
+        QuorumPeerConfig quorumConfiguration = new QuorumPeerConfig();
+        try {
+            quorumConfiguration.parseProperties(startupProperties);
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        ZooKeeperServerMain zooKeeperServer = new ZooKeeperServerMain();
+        final ServerConfig configuration = new ServerConfig();
+        configuration.readFrom(quorumConfiguration);
+
+        new Thread() {
+            public void run() {
+                try {
+                    zooKeeperServer.runFromConfig(configuration);
+                } catch (IOException e) {
+//                    log.error("ZooKeeper Failed", e);
+                }
+            }
+        }.start();
+    }
+}
